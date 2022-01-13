f10e2f6538fa15b77cc3693fbe1201e92441400c
Zvonty Flugence <zvontyflugence@gmail.com>
2022-01-13 01:31:41 -0500

    Prevent rendering of Dashboard until user state is loaded

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { useUser } from '@/core/context/UserContext'
 import { Box, Heading, SimpleGrid } from '@chakra-ui/layout'
 import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/react'
@@ -5,44 +6,48 @@ import Dailies from '../components/dashboard/Dailies'
 import Layout from '../components/layout/Layout'
 
 const Dashboard: React.FC = () => {
-    return (
-        <Layout>
-            <div className='flex flex-col w-full'>
-                <Heading color='aurora.red.500' className='text-2xl font-semibold'>Dashboard</Heading>
-                <div className='hidden md:block'>
-                    <SimpleGrid w='full' columns={2} spacing={12}>
-                        <Box display='flex' flexDir='column' gap={12}>
-                            <Dailies />
-                            <div>News here</div>
-                        </Box>
-                        <Box>
-                            <div>Shouts here</div>
-                        </Box>
-                    </SimpleGrid>
-                </div>
-                <div className='block md:hidden'>
-                    <Dailies />
-                    <div>News here</div>
-                    <div>Shouts here</div>
-                </div>
-            </div>
-        </Layout>
-    )
+	const user = useUser()
+
+	return user ? (
+		<Layout>
+			<div className='flex flex-col w-full'>
+				<Heading color='aurora.red.500' className='text-2xl font-semibold'>
+					Dashboard
+				</Heading>
+				<div className='hidden md:block'>
+					<SimpleGrid w='full' columns={2} spacing={12}>
+						<Box display='flex' flexDir='column' gap={12}>
+							<Dailies />
+							<div>News here</div>
+						</Box>
+						<Box>
+							<div>Shouts here</div>
+						</Box>
+					</SimpleGrid>
+				</div>
+				<div className='block md:hidden'>
+					<Dailies />
+					<div>News here</div>
+					<div>Shouts here</div>
+				</div>
+			</div>
+		</Layout>
+	) : null
 }
 
 export default Dashboard
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const { req } = context
-    const session = await getSession({ req })
+	const { req } = context
+	const session = await getSession({ req })
 
-    if (!session) {
-        return {
-            redirect: { destination: '/login', permanent: false }
-        }
-    }
+	if (!session) {
+		return {
+			redirect: { destination: '/login', permanent: false },
+		}
+	}
 
-    return {
-        props: {}
-    }
+	return {
+		props: {},
+	}
 }
